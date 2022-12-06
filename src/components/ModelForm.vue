2a8fd46789494aae03ca5df00cd4ff2e5291e4d7
= <=>
2022-12-05 23:46:03 -0500

    v-model con inuput tipo checkbox y select con el option API

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -1,6 +1,12 @@
 <template>
     <form>
         <input type="text" v-model="inputText">
+        <input type="checkbox" v-model="agree">
+        <select v-model="teams">
+            <option value="ManUtd">Manchester United</option>
+            <option value="Barca">Barcelona</option>
+            <option value="Milan">Milan</option>
+        </select>
     </form>
 
     <h3>{{inputText}}</h3>
@@ -11,7 +17,7 @@
 export default defineComponent({
     name:"ModelForm",
     data(){
-        return {inputText:""}
+        return {inputText:"",agree:true,teams:"ManUtd"}
     }
 })
 </script>
